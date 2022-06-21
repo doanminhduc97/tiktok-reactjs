cdabaa76523eb21713bee726f423cba467ec9af4
doanminhduc1007 <ducshooter.luvina@gmail.com>
2022-06-21 21:45:17 +0700

    xay dung base + ui header phan 1

---
+++ src/components/Layout/HeaderOnly/index.js
@@ -0,0 +1,14 @@
+import Header from '../components/Header';
+
+function HeaderOnly({ children }) {
+  return (
+    <div>
+      <Header />
+      <div className="container">
+      </div>
+      <div className="content"> {children} </div>
+    </div>
+  );
+}
+
+export default HeaderOnly;
